72cb41cfba014dcde28806e1ebf367d46f2c8fb2
Futa Hirakoba <korosuke613613@gmail.com>
2018-02-21 05:54:44 +0900

    200ページまで修了

---
+++ sec4/list4-5.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var ejs = require('ejs');
+
+var app = express();
+app.engine('ejs', ejs.renderFile);
+
+app.get('/', (req, res) =>{
+    // index.ejsをレンダリングする
+    res.render('list4-4.ejs',{
+        title: 'Index',
+        content: 'This is Express-app Top page!'});
+});
+
+app.listen(3000, ()=>{
+    console.log('Start server port:3000');
+});
